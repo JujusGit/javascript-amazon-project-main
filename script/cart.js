4182c0ab41f90dd8961e2d8f9959022bdb0cd803
dudu <alhassanmuhammad714@gmail.com>
2024-11-13 23:11:35 -0800

    learn promises and convert code to promises

--- script/cart.js
+++ script/cart.js
@@ -95,3 +95,16 @@ cart.forEach((cartItem)=>{
 matchingitem.optionsId = optionsId;
 saveToStorae()
 }
+
+
+export function loadCart(fun){
+  const xhr =  new XMLHttpRequest();
+ 
+  xhr.addEventListener('load',  () => {
+   console.log(xhr.response);
+   
+   fun();
+  })
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+ }
--- script/checkout.js
+++ script/checkout.js
@@ -1,15 +1,62 @@
 import {renderOrderSummery} from './checkout/orderSummery.js';
 import {renderPaymentsummery} from './checkout/paymentSummery.js';
-import { cart } from './cart.js';
+import { cart, loadCart } from './cart.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js'
 import { loadProducts  } from '../data/products.js';
 //import '../data/cart-class.js';
 
-loadProducts(() => {
+
+Promise.all(
+  [
+    new Promise((resolve) => {
+      loadProducts(() => {
+        resolve();
+      });
+    }),
+
+
+    new Promise((resolve) => {
+      loadCart(()=> {
+        resolve()
+      })
+    })
+  ]
+).then(()=>{
   renderCheckoutHeader(cart);
   renderPaymentsummery();
   renderOrderSummery();
 })
 
 
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+})
+
+.then(() => {
+  return new Promise((resolve) => {
+    loadCart(()=> {
+      resolve()
+    })
+  })
+})
+
+.then(()=>{
+  renderCheckoutHeader(cart);
+  renderPaymentsummery();
+  renderOrderSummery();
+})
+  */
+ 
+/*
+loadProducts(() => {
+  loadCart(()=> {
+    renderCheckoutHeader(cart);
+    renderPaymentsummery();
+    renderOrderSummery();
+  });
+});
+*/
 
